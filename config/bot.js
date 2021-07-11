077dd60fb200fc4575dbe7e73ea4f0be9be0a93c
Glitch (wood-piquant-honeysuckle) <none>
2021-07-11 16:40:27 +0000

    Import of han-han030/han-music2.0

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODU3NTQ2MDIxMjIzMTM3MzAx.YNRJ9w.N52ImYSEpWqJ_T-uc6Yw_y_Svec',
+        prefix: 'han!',
+        activity: 'ACTIVITY',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
