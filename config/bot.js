e8892b00ac373ca4b56d63e13a91206261140712
SUSLEH <97380492+SUSLEH@users.noreply.github.com>
2022-01-30 15:39:47 +0800

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: ' ',
-        prefix: 'han!',
-        activity: 'ACTIVITY',
+        token: 'OTM1MTA5MjQ5MzA5MjQ1NDUw.Ye52Wg.hKpoMVNoXdOZv9XLWx_o21_SU50',
+        prefix: 'ee',
+        activity: 'SLEEPING',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
